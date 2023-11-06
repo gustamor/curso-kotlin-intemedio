1d9858bac5b0083c07706be6c1c6b9f63a3f2621
Gustavo Moreno <gustavo.moreno.fernandez@gmail.com>
2023-11-06 15:43:42 +0100

    added luck fragment image animation

--- app/src/main/java/com/example/horrorscope/ui/luck/LuckFragment.kt
+++ app/src/main/java/com/example/horrorscope/ui/luck/LuckFragment.kt
@@ -1,20 +1,52 @@
 package com.example.horrorscope.ui.luck
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.DecelerateInterpolator
 import com.example.horrorscope.databinding.FragmentLuckBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.Random
 
 @AndroidEntryPoint
 class LuckFragment : Fragment() {
 
-
-
     private var _binding: FragmentLuckBinding ? = null
     private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
+
+    private fun initUI() {
+        initListeners()
+    }
+
+    private fun initListeners() {
+        binding.ivRoulette.setOnClickListener {
+            spinRoulette()
+        }
+    }
+
+    private fun spinRoulette() {
+
+        val random = Random()
+        val degrees = random.nextInt(360*4) + 360
+
+        val animator = ObjectAnimator.ofFloat(binding.ivRoulette,
+            View.ROTATION,
+            0f,
+            degrees.toFloat()
+        )
+        animator.duration = 2000
+        animator.interpolator =  DecelerateInterpolator()
+        animator.start()
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
